e702a26904151c5a8d4d084663f105a3948fe7a1
Siyun37 <tldbs37@gmail.com>
2026-08-20 18:35:06 +0900

    Dockerfile 다시 추가 ...

--- src/main/java/com/mcm/craft/CraftApplication.java
+++ src/main/java/com/mcm/craft/CraftApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CraftApplication {
+public class
+CraftApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CraftApplication.class, args);
